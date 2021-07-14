ced45e689bdec7fdf0fd3c3ef8b8b84a7bb15cf3
Alan Whitman <parsimonious.one@gmail.com>
2021-07-14 12:29:38 -0600

    get_next_instruction, validate_program in and functional. Changes to uvsim.py to conform to spec

--- uvsim.py
+++ uvsim.py
@@ -5,7 +5,13 @@ def main():
     bml.initial_prompt()
     while bml.get_next_instruction():
         pass
-    bml.run_program()
+    print("*** Loading Program... ***")
+    program_is_valid = bml.validate_program()
+    if program_is_valid:
+        print("*** Program loading completed ***\n*** Program execution begins ***")
+        bml.run_program()
+    else:
+        print("*** Program contains invalid instructions. Unable to execute ***")
     bml.dump()
 
 if __name__ == "__main__":
